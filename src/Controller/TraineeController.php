4e3d4357f66c1b869ed223cba2cd1fc56bf8ad41
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-10-22 15:02:34 +0200

    added a show view or every trainee with all their details

--- src/Controller/TraineeController.php
+++ src/Controller/TraineeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Trainee;
+use App\Repository\TraineeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,11 +12,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class TraineeController extends AbstractController
 {
     #[Route('/trainee', name: 'app_trainee')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(TraineeRepository $traineeRepository): Response
     {
-        $trainees = $entityManager->getRepository(Trainee::class)->findAll();
+        $trainees = $traineeRepository->findBy([], ["firstname" => "ASC"]);
         return $this->render('trainee/index.html.twig', [
             'trainees' => $trainees
+        
         ]);
     }
+
+    #[Route('/trainee/{id}', name: 'show_trainee')]
+    public function show(Trainee $trainee): Response 
+    {
+        return $this->render('trainee/show.html.twig', [
+            'trainee' => $trainee
+        ]);
+    }
+
 }
