515494ad1d94c3887c27b0b2b7384f5819a9b746
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-10-22 14:35:15 +0200

    listed all the sessions

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Session;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionController extends AbstractController
 {
     #[Route('/session', name: 'app_session')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $sessions = $entityManager->getRepository(Session::class)->findAll();
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'sessions' => $sessions
         ]);
     }
 }
